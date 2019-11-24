220b59e7b61dce4575f7cfac58a2ef9ada28b022
bharvee2001 <bharvee.parmar@gmail.com>
2019-11-24 13:16:35 +0530

    commnet added by sumit

--- CurrenctLocationGoogleMap/ViewController.swift
+++ CurrenctLocationGoogleMap/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // this is test by bharvee
+       // this is sumit
     }
 
 
